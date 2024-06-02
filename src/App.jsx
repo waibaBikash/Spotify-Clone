c352562b08879e2f3346b7540191a9ecd5416622
waibaBikash <kavrelibikash@gmail.com>
2024-06-03 08:37:40 +1000

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import Display from './components/Display';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className='h-[90%] flex'>
         <Sidebar />
         <Player />
+        <Display />
       </div>
     </div>
   )
---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = () => {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default Display;
