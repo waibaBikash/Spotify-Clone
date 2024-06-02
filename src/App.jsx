8496c5000bb501c5c980f190cbcdcea5bc906774
waibaBikash <kavrelibikash@gmail.com>
2024-06-03 01:29:54 +1000

    create plyer ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Player />
       </div>
     </div>
   )
