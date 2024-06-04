13ae62d8a52b13103eee6d4d3c15b7a1bbe4e998
waibaBikash <kavrelibikash@gmail.com>
2024-06-04 20:17:44 +1000

    hiding scroll bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
            <div className='flex items-center gap-4'>
                <p className='bg-white text-black tixt-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Primium</p>
                <p className=' bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
-               <p className='bg-purple-500 text-black w-7 rounded-full flex items-center justify-center'>D</p>
+               <p className='bg-purple-500 text-black w-7 rounded-full flex items-center justify-center'>G</p>
            </div>
       </div>
        <div className="flex items-center gap-2 mt-4  ">
