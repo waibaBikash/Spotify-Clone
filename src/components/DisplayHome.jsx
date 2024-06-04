ff867c32ba4eeba2bb34958846cba0a2db58e5ef
waibaBikash <kavrelibikash@gmail.com>
2024-06-04 19:01:30 +1000

    create a playlist

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { albumsData } from '../assets/assets';
+import AlbumItem from './AlbumItem';
 
 const DisplayHome = () => {
   return (
     <>
    < Navbar />
-     <div className='my-5 font-bold text-2xl'>
-
+     <div className='mb-4'>
+         <h1 className='my-5 font-bold text-2xl'>Feature Charts</h1>
+         <div className='flex overflow-auto'>
+         {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))} 
+         </div>
      </div>
     </>
   )
