9bafdcf1a1269bd1b40fd42ae4ed988004eb9b49
waibaBikash <kavrelibikash@gmail.com>
2024-06-04 20:30:45 +1000

    create a songs list

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { albumsData } from '../assets/assets';
+import { albumsData, songsData } from '../assets/assets';
 import AlbumItem from './AlbumItem';
+import SongItem from './SongItem';
 
 const DisplayHome = () => {
   return (
@@ -11,6 +12,12 @@ const DisplayHome = () => {
          <h1 className='my-5 font-bold text-2xl'>Feature Charts</h1>
          <div className='flex overflow-auto'>
          {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))} 
+         </div>
+     </div>
+     <div className='mb-4'>
+         <h1 className='my-5 font-bold text-2xl'>Today's bigest hits</h1>
+         <div className='flex overflow-auto'>
+         {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))} 
          </div>
      </div>
     </>
