69ee2fb0b580b7977e6b50375ce51a0191b1a32d
waibaBikash <kavrelibikash@gmail.com>
2024-06-04 21:17:54 +1000

    compte button functionality

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -39,6 +39,9 @@ const DisplayAlbum = () => {
             <img className='inline w-10 mr-5' src={item.image} alt="" />
             {item.name}
           </p>
+          <p className='text-[15px]'>{albumData.name}</p>
+          <p className='text-[15px] hidden sm:block '>3 days ago</p>
+          <p className='text-[15px] text-center'>{item.duration}</p>
          </div>
        ))}
     </>
