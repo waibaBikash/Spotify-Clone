deff448d97766e828aef48e32f884b5fe7bff042
waibaBikash <kavrelibikash@gmail.com>
2024-06-04 21:09:24 +1000

    Create Music Album Page

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -6,7 +6,6 @@ const SongItem = ({name,image,desc,id}) => {
       <img src={image} alt="" />
       <p className='font-bold mt2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
-
     </div>
   )
 }
